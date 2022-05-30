9b3ec597dd06913440cb4ba18b35e121e1ead0c2
Mikael Nilsson <Micke.N90@hotmail.com>
2022-05-30 14:06:46 +0200

    asd

--- HamsterWars/Server/Controllers/HamsterGalleryController.cs
+++ HamsterWars/Server/Controllers/HamsterGalleryController.cs
@@ -1,6 +1,7 @@
 ﻿using HamsterWars.Server.DataBase;
 using HamsterWars.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HamsterWars.Server.Controllers
 {
@@ -19,68 +20,104 @@ namespace HamsterWars.Server.Controllers
         // GET: api/<HamsterController>
         [HttpGet("hamsters")]
         
-        public IEnumerable<Hamster> Get()
+        public async Task<ActionResult<IEnumerable<Hamster>>> Get()
         {
-            List<Hamster> hamsters = context.Hamsters.ToList();              
-                return hamsters;
+            List<Hamster> hamsters = await context.Hamsters.ToListAsync();
+            if (hamsters.Count > 0)
+                return Ok(hamsters);
+            else
+                return NotFound("No Hamsters Found");
            
                
         }
         [HttpGet("hamsters/random")]
-        public Hamster GetRandom()
+        public async Task<ActionResult<Hamster>> GetRandom()
         {
-            List<Hamster> hamsters = context.Hamsters.ToList();
-            Random random = new Random();           
-            return hamsters[random.Next(0, hamsters.Count())];
+            List<Hamster> hamsters = await context.Hamsters.ToListAsync();
+            if (hamsters.Count > 0)
+            {
+                Random random = new Random();
+                return Ok(hamsters[random.Next(0, hamsters.Count())]);
+            }
+            else return NotFound("No Hamsters Found");
         }
 
         // GET api/<HamsterController>/5
         [HttpGet("hamsters/{id}")]
-        public Hamster GetOne(int id)
+        public async Task<ActionResult<Hamster>> GetOne(int id)
         {
-            Hamster hamster = context.Hamsters.Find(id);
-            return hamster;
+            try
+            {
+                Hamster hamster = await context.Hamsters.FindAsync(id);
+                if (hamster == null) return BadRequest("No Hamster Found!");
+                return Ok(hamster);
+            }
+            catch (Exception ex)
+            {
+               return NotFound(ex.Message + "Fann inte objektet");
+            }
         }
 
         // POST api/<HamsterController>
         [HttpPost]
         public async Task<ActionResult<List<Hamster>>> AddHamster([FromBody] Hamster hamster)
         {
-            context.Add(hamster);
-            context.SaveChanges();
-            return Ok(hamster);
+            try
+            {
+                context.Add(hamster);
+                context.SaveChanges();
+                return Created("HamsterGallery", hamster);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message + "Somthing Wrong!");
+            }
         }
 
         // PUT api/<HamsterController>/5
         [HttpPut("hamsters/{id}")]
         public async Task<ActionResult<List<Hamster>>> UpdateHamster(int Id, [FromBody] Hamster request)
         {
-
-            //INTE KLAR
-            var findHamster = context.Hamsters.Find(request.Id);
-            //var findHamsterA = context.Hamsters.Find(x => x.id == Id);
-            if (findHamster == null)
-                return BadRequest("Hamster not found");
-            findHamster.Name = request.Name;
-            findHamster.Age = request.Age;
-            findHamster.Loves = request.Loves;
-            findHamster.Games = request.Games;
-            findHamster.Wins = request.Wins;
-            findHamster.Losses = request.Losses;
-            context.Update(findHamster);
-            context.SaveChanges();
-             return Ok(request);
+            try
+            {
+                var findHamster = context.Hamsters.Find(request.Id);
+                //var findHamsterA = context.Hamsters.Find(x => x.id == Id);
+                if (findHamster == null)
+                    return BadRequest("Hamster not found");
+                findHamster.Name = request.Name;
+                findHamster.Age = request.Age;
+                findHamster.Loves = request.Loves;
+                findHamster.Games = request.Games;
+                findHamster.Wins = request.Wins;
+                findHamster.Losses = request.Losses;
+                context.Update(findHamster);
+                context.SaveChanges();
+                return Accepted(request);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Gick inte att uppdatera!" + ex.Message);
+            }
         }
 
         // DELETE api/<HamsterController>/5
         [HttpDelete("hamsters/{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            Hamster hamster = context.Hamsters.Find(id);
-            if (hamster != null)
+            try
             {
-                context.Hamsters.Remove(hamster);
-                context.SaveChanges();
+                Hamster hamster = context.Hamsters.Find(id);
+                if (hamster != null)
+                {
+                    context.Hamsters.Remove(hamster);
+                    await context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
     }
