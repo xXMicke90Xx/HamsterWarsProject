f007914982713d83b85694f3a151f8650e632dfb
Robin Liukkonen <zn_1996@hotmail.com>
2022-05-31 14:51:12 +0200

    231213

---
+++ HamsterWars/Server/Controllers/FileUploadsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HamsterWars.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] IFormFile image)
+        {
+            if (image == null || image.Length == 0) return BadRequest("Upload any Image");
+
+            string fileName = image.FileName;
+            string extensions = Path.GetExtension(fileName);
+
+
+            string[] allow = { ".jpg", ".png" };
+
+            if (!allow.Contains(extensions.ToLower())) return BadRequest("Invalid Image");
+
+            string fileNameNew = $"{Guid.NewGuid()}{extensions}";
+            string filePath = Path.Combine(_webHostEnvironment.ContentRootPath, "wwwroot", "Images", "hamsters", fileNameNew);
+
+            using(var filestream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                await image.CopyToAsync(filestream);
+            }
+
+            return Ok($"Files/{fileNameNew}");
+        }
+    }
+}
