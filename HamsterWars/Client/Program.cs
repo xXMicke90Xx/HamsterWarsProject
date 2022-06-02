aebf16140cc3673e8fea9a6797e6b226764b5f44
Mikael Nilsson <Micke.N90@hotmail.com>
2022-06-02 09:02:34 +0200

    asd

--- HamsterWars/Client/Program.cs
+++ HamsterWars/Client/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFileReaderService();
-builder.Services.AddScoped<HttpClient>();
+
 
 await builder.Build().RunAsync();
